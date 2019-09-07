e4d3f324eca28d5f5b9202f945691551cae75c88
bartholomews <fbarto01@mail.bbk.ac.uk>
2019-09-07 22:35:07 +0100

    GitLab CI - build stage WIP

--- scripts/deploy-docker-image.sh
+++ scripts/deploy-docker-image.sh
@@ -4,9 +4,9 @@ VERSION=$(./scripts/read-artifact-name.py)
 VERSIONED=bartholomews/keyaki:${VERSION}
 LATEST=bartholomews/keyaki:latest
 
-#docker tag bartholomews/keyaki ${VERSIONED}
+docker tag bartholomews/keyaki ${VERSIONED}
 echo pushing ${VERSIONED} to registry
-#docker push ${VERSIONED}
-#docker tag bartholomews/keyaki ${LATEST}
+docker push ${VERSIONED}
+docker tag bartholomews/keyaki ${LATEST}
 echo pushing ${LATEST} to registry
-#docker push ${LATEST}
+docker push ${LATEST}
