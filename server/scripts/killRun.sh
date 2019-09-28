a39aa5a03a175804f77a0ad2ed0c1fd093e6363a
bartholomews <fbarto01@mail.bbk.ac.uk>
2019-09-28 14:20:54 +0100

    kill -9 pgrep -> pkill

--- server/scripts/killRun.sh
+++ server/scripts/killRun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kill -9 `pgrep keyaki`
+pkill keyaki
 stack exec keyaki &
 sleep 0.6
 echo -e "server running with PID={`pgrep keyaki`} (if that's empty, then the server is not running)"
