7fe971b542ed6ff722ebaec83a2d8af84b5a49c5
bartholomews <fbarto01@mail.bbk.ac.uk>
2019-09-28 14:21:07 +0100

    docker one-line command

--- scripts/docker-build-local.sh
+++ scripts/docker-build-local.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 # https://dzone.com/articles/shrinking-haskell-docker-images-using-multi-stage
-docker build -t bartholomews/keyaki \
-#    --build-arg random=${RANDOM} \
-    --target build-prod \
-    -f docker/local/Dockerfile .
+docker build -t bartholomews/keyaki --target build-prod -f docker/local/Dockerfile .
