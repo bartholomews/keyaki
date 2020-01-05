0194ff1e239d55c9270829dbdca8c73159c523b7
bartholomews <git@bartholomews.io>
2020-01-05 22:55:00 +0000

    `before_script` commands

--- scripts/docker-credential-pass.sh
+++ scripts/docker-credential-pass.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 
-DOCKER_PASS=docker-credential-pass-v0.6.3-amd64.tar.gz
-PASS=password-store-1.7.3
-
-########################################################################################################################
-# Fetch and install `pass` and `docker-credential-pass`
-########################################################################################################################
-apk add --update libarchive-tools make bash duplicity
-wget https://github.com/docker/docker-credential-helpers/releases/download/v0.6.3/${DOCKER_PASS}
-tar -xf ${DOCKER_PASS}
-wget https://git.zx2c4.com/password-store/snapshot/${PASS}.tar.xz
-bsdtar xfJ ${PASS}.tar.xz
-cd ${PASS} && make install
-mv docker-credential-pass /usr/bin
-chmod 100 /usr/bin/docker-credential-pass
-########################################################################################################################
+set -euxo pipefail
 
 DOCKER_CONFIG=${HOME}/.docker/config.json
 
