454d15db61c9edb377bbfd1905113739ebc13d25
bartholomews <git@bartholomews.io>
2020-01-05 23:44:56 +0000

    Retrying with pub in var

--- scripts/docker-credential-pass.sh
+++ scripts/docker-credential-pass.sh
@@ -4,20 +4,15 @@ set -euxo pipefail
 
 DOCKER_CONFIG=${HOME}/.docker/config.json
 
-mkdir ${HOME}/.docker && touch ${HOME}/.docker/config.jso
-echo "{ \"credsStore\": \"pass\" }" > ${HOME}/.docker/config.json
-
-ls
-ls docker
-
-cat docker/GPG_PUB
-gpg --import docker/GPG_PUB
-
-ls
+#gpg --import docker/GPG_PUB
+gpg --import ${GPG_PUB}
 
 # https://unix.stackexchange.com/questions/184947/how-to-import-secret-gpg-key-copied-from-one-machine-to-another
 touch ~/.gnupg/gpg.conf && echo "pinentry-mode loopback" >> ~/.gnupg/gpg.conf
 gpg --no-tty --passphrase "${GPG_PASSPHRASE}" --import "${GPG_PEM}"
+
+mkdir ${HOME}/.docker && touch ${HOME}/.docker/config.jso
+echo "{ \"credsStore\": \"pass\" }" > ${HOME}/.docker/config.json
 
 pass init ${GPG_ID}
 docker-credential-pass list
