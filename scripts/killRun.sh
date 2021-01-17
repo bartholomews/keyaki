b66c49de442d5b86d63eb50513c22e37b96dd19b
bartholomews <github@bartholomews.io>
2021-01-17 10:23:05 +0000

    env double quotes

--- scripts/killRun.sh
+++ scripts/killRun.sh
@@ -2,4 +2,4 @@
 pkill keyaki
 stack exec keyaki &
 sleep 0.6
-echo -e "server running with PID={`pgrep keyaki`} (if that's empty, then the server is not running)"
+echo -e "server running with PID={$(pgrep keyaki)} (if that's empty, then the server is not running)"
